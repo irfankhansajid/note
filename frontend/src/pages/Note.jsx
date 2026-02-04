f2733e6d3fea559ca565d9c5ced610df64be45f7
irfankhansajid <irfankhansajid@gmail.com>
2026-02-04 16:22:39 +0600

    complete crud full add, delete, update and get

--- frontend/src/pages/Note.jsx
+++ frontend/src/pages/Note.jsx
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react";
-import { fetchMyNotes } from "../api/notes";
+import { createNote, deleteNote, fetchMyNotes, updateNote } from "../api/notes";
 import Login from "./Login";
 
 
 export default function Note() {
     const [notes, setNotes] = useState([]);
     const token = localStorage.getItem('token');
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const [editingId, setEditingId] = useState(null);
+    const [editTitle, setEditTitle] = useState('');
+    const [editContent, setEditContent] = useState('');
 
     useEffect(() => {
         if (token) {
@@ -27,15 +34,87 @@ export default function Note() {
 
     if (!token) return <Login/>
 
+
+    const handleCreate = async (e) => {
+        e.preventDefault();
+        try {
+            const newNote = await createNote({title, content});
+            setNotes([...notes, newNote]);
+            setTitle('')
+            setContent('')
+        } catch(err) {
+            alert(err.message);
+        }
+    }
+
+    const handleDelete = async (id) => {
+        if (window.confirm("Delete this note?")) {
+            await deleteNote(id);
+            setNotes(notes.filter(note => note.id !== id)); 
+        }
+    };
+
+    const handleUpdate = async (id) => {
+        try {
+            const updated = await updateNote(id, {title: editTitle, content: editContent});
+            setNotes(notes.map(n => n.id === id ? updated : n))
+            setEditingId(null)
+        } catch(err) {
+            alert(err.message); 
+        }
+    }
+    const startEdit = (note) => {
+        
+        setEditingId(note.id)
+        setEditTitle(note.title)
+        setEditContent(note.content)
+    }
+
+
     return (
         <div>
             <h1>My notes from (cloud)</h1>
+
+            <form onSubmit={handleCreate} style={{ marginBottom: '20px' }}>
+                <input value={title} placeholder="Title" onChange={e => setTitle(e.target.value)} required />
+                <textarea value={content} placeholder="Content" onChange={e => setContent(e.target.value)} required />
+                <button type="submit">Add Note</button>
+            </form>
+
             <ul>
                 {notes && notes.length > 0 ? (
                     notes.map(note => (
                     <li key={note.id}>
+                        {editingId == note.id ? (
+                            <div style={{ background: '#f9f9f9', padding: '10px' }}>
+                        <input 
+                            value={editTitle} 
+                            onChange={(e) => setEditTitle(e.target.value)} 
+                            style={{ display: 'block', marginBottom: '5px' }}
+                        />
+                        <textarea 
+                            value={editContent} 
+                            onChange={(e) => setEditContent(e.target.value)} 
+                            style={{ display: 'block', marginBottom: '5px' }}
+                        />
+                        <button onClick={() => handleUpdate(note.id)}>Save</button>
+                        <button onClick={() => setEditingId(null)}>Cancel</button>
+                    </div>
+                        ) : (
+                            <div>
                         <strong>{note.title}</strong>
                         <p>{note.content}</p>
+                        <button onClick={() => startEdit(note)}>Edit</button>
+                        <button 
+                            onClick={() => handleDelete(note.id)} 
+                            style={{ color: 'red', marginLeft: '10px' }}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                        )}
+                        
+                    
                     </li>
                 ))
             ) : (
